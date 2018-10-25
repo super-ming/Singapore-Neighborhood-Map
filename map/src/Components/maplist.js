067e03a92979fbefcc1c15c8ea53b82c0bb10aef
Ming Ho <hkboi108@hotmail.com>
2018-10-25 11:41:06 -0700

    create maplist dynamically

--- map/src/Components/maplist.js
+++ map/src/Components/maplist.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 
 class MapList extends Component {
   render() {
+    const locations = this.props.places;
+    console.log(locations);
     return (
-      <div></div>
+      <div className="place-list">
+      { locations.map((place, index) =>
+        <div className="list-item" key={index}>{place.name}</div>
+      )}
+      </div>
     );
   }
 }
