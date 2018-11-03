e18e5626ad0dea149e100eb55435acbc6cf29315
Ming Ho <hkboi108@hotmail.com>
2018-11-03 11:38:00 -0700

    update venuelist component to stateless functional component

--- map/src/Components/venuelist.js
+++ map/src/Components/venuelist.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class VenueList extends Component {
-  render() {
-    const venues = this.props.venues;
-    return (
-      <div>
-        <input id="searchBox" type="text" placeholder="Type here to filter locations" value={this.props.query}
-        onChange={event => this.props.updateQuery(event.target.value)}
-        onKeyUp={event => this.props.updateQuery(event.target.value)}/>
-        <div className="place-list">
-        { venues && (venues.map((venue, index) =>
-          <div className="list-item" name={venue.name} key={index}
-          onClick={event => this.props.onListClick(venue, index)}>{venue.name} </div>
-        ))}
-        </div>
+const VenueList = (props) => {
+  const venues = props.venues;
+  return (
+    <div>
+    <label htmlFor="searchBox">Search Box</label>
+      <input
+        id="searchBox"
+        type="text"
+        placeholder="Type here to filter locations"
+        value={props.query}
+        onChange={event => props.updateQuery(event.target.value)}
+        onKeyUp={event => props.updateQuery(event.target.value)}/>
+      <div className="place-list">
+      { venues && (venues.map((venue, index) =>
+        <div
+          className="list-item"
+          name={venue.name} key={index}
+          onClick={event => props.onListClick(venue, index)}>{venue.name} </div>
+      ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 export default VenueList;
