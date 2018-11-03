6acdb240d1b9a31443f750bdc56103aee9d8a0d4
Ming Ho <hkboi108@hotmail.com>
2018-11-02 23:13:15 -0700

    rename filename

--- map/src/Components/venuelist.js
+++ map/src/Components/venuelist.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 
-class MapList extends Component {
+class VenueList extends Component {
   render() {
-    const locations = this.props.places;
+    const venues = this.props.venues;
     return (
       <div>
         <input id="searchBox" type="text" placeholder="Type here to filter locations" value={this.props.query}
         onChange={event => this.props.updateQuery(event.target.value)}
         onKeyUp={event => this.props.updateQuery(event.target.value)}/>
         <div className="place-list">
-        { locations && (locations.map((place, index) =>
-          <div className="list-item" name={place.name} key={index}
-          onClick={event => this.props.onListClick(place, index)}>{place.name} </div>
+        { venues && (venues.map((venue, index) =>
+          <div className="list-item" name={venue.name} key={index}
+          onClick={event => this.props.onListClick(venue, index)}>{venue.name} </div>
         ))}
         </div>
       </div>
     );
   }
 }
-export default MapList;
+export default VenueList;
