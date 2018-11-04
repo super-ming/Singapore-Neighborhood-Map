fa561e3841d50d54af8b8f758db16530d99761c6
Ming Ho <hkboi108@hotmail.com>
2018-11-04 11:56:40 -0800

    Updates

---
+++ precache-manifest.5fc9587a4c94c4e78dc751299662d9ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e0c17a23156a1ab11be",
+    "url": "/super-ming/Neighborhood-Map/static/js/runtime~main.8e0c17a2.js"
+  },
+  {
+    "revision": "05968e4b9e8e4a92a19c",
+    "url": "/super-ming/Neighborhood-Map/static/js/main.05968e4b.chunk.js"
+  },
+  {
+    "revision": "2d52335f32db02112247",
+    "url": "/super-ming/Neighborhood-Map/static/js/1.2d52335f.chunk.js"
+  },
+  {
+    "revision": "05968e4b9e8e4a92a19c",
+    "url": "/super-ming/Neighborhood-Map/static/css/main.76c3bc7c.chunk.css"
+  },
+  {
+    "revision": "a81172b8bb706886cf56af24ebc57682",
+    "url": "/super-ming/Neighborhood-Map/index.html"
+  }
+];
