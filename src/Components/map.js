f1ff0f5199a24e4b744cb7fb04a9e0ace5687cdd
Ming Ho <hkboi108@hotmail.com>
2018-11-06 11:55:20 -0800

    reuse config.js for hosting

--- src/Components/map.js
+++ src/Components/map.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import ErrorBoundary from './errorboundary';
+import { storeAPI } from '../config';
 
 window.gm_authFailure = ()=>{
   alert("Invalid Google API key. Please check your Google API key");
 };
 
-const fbAppID = process.env.REACT_APP_fbAppID;
-const fbAppSecret = process.env.REACT_APP_fbAppSecret;
-const googleMapsAPI = process.env.REACT_APP_googleMapsAPI;
+const fbAppID = storeAPI().fbAppID;
+const fbAppSecret = storeAPI().fbAppSecret;
+const googleMapsAPI = storeAPI().googleMapsAPI;
 
 class MapContainer extends Component {
   componentDidMount() {
